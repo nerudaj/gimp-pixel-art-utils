6ea3ceb7227df3586338da00aee62a8d3bbd1852
Jakub Neruda <nerudaj@email.cz>
2022-07-28 17:31:07 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # gimp-layer-group-animation-preview
- Plugin for previewing animations stored in a layer group.
+
+Plugin for previewing animations stored in a layer group. This plugin is meant for pixel artists who create tilesets for the game and works well in conjunction with [tilemancer](https://github.com/nerudaj/tilemancer)
+
+## How to install
+
+Open Gimp, go to Edit->Preferences->Folders->Plug-Ins and open one of the listed folders. Copy the folder containing the .py file to the opened `plug-ins` folder and restart Gimp. Now, under the `Windows` menu you should see Layer Group Animation Preview.
+
+## How to use
+
+This plugin is aimed at the users of tilemancer. This guide expects you have installed tilemancer into Gimp before. Expected workflow is such that you have any number of layer groups, some of those groups containing animations, others might only contain some tiles/one shot textures.
+
+You can preview any animation just by launching this plugin and selecting name of appropriate group from the dropdown menu. Animation is played automatically in an endless cycle, showing you live preview, so you can let it play in a corner of the screen and keep working on your animation, even adding new frames etc. You can also change FPS of the animation just by editing appropriate box and clicking `Update`.
+
+When you are done with your tileset, use Filters->Animations->Tilemancer plugin, choose "One row per group" shape and confirm. Finished tileset will be created into separate image.
+
+> NOTE: Tilemancer exports everything in reverse, so first item in the group will be the first item on a row. For this reason, animation preview also plays first item as the last one. This is consistent with Gimp's behaviour when exporting GIFs.
---
+++ layer-group-animation-preview/layer-group-animation-preview.py
@@ -0,0 +1,161 @@
+#! /usr/bin/python
+
+from gimpfu import *
+import gtk
+import gimpui
+import gobject
+
+# global state
+window = None
+window_box = None
+display_box = None
+preview_box = None
+layers_list = None
+active_layer = None
+current_frame = None
+frame_index = 0
+fps = 16
+
+def null_callback():
+    print("null_callback called")
+
+def update_fps(widget, entry):
+    pdb.gimp_message("got here")
+    global fps
+    fps_text = entry.get_text()
+    fps = int(fps_text)
+    pdb.gimp_message("New fps: {}".format(fps))
+
+def create_preview_window():
+    pdb.gimp_message("create_preview_window start")
+
+    global window
+    global window_box
+    global current_frame
+    global layers_list
+    global display_box
+    global fps
+
+    window = gtk.Window()
+    window.set_title("Animation preview")
+    window.connect('destroy',  close_preview_window)
+    window_box = gtk.VBox()
+    window.add(window_box)
+
+    display_box = gtk.HBox()
+    window_box.pack_start(display_box, True, True, 0)
+
+    button_box = gtk.HBox()
+    window_box.pack_start(button_box, True, True, 0)
+
+    fps_entry_label = gtk.Label("FPS")
+    button_box.pack_start(fps_entry_label, True, True, 0)
+
+    fps_entry = gtk.Entry()
+    fps_entry.set_text("{}".format(fps))
+    button_box.pack_start(fps_entry, True, True, 0)
+
+    btn = gtk.Button()
+    btn.set_label("Update")
+    btn.connect("clicked", update_fps, fps_entry)
+    button_box.pack_start(btn, True, True, 0)
+
+    # ComboBox for selecting available layers with animations
+    combox = gtk.combo_box_new_text()
+    combox.connect("changed", active_layer_changed)
+    for layer in layers_list:
+        pdb.gimp_message(layer.name)
+        combox.append_text(layer.name)
+    combox.set_active(0)
+    button_box.pack_start(combox, True, True, 0)
+
+    window.show_all()
+    update_preview()
+
+def active_layer_changed(combo):
+    pdb.gimp_message("active_layer_changed to {}".format(combo.get_active_text()))
+
+    global layers_list
+    global active_layer
+    global frame_index
+
+    for layer in layers_list:
+        if layer.name == combo.get_active_text():
+            pdb.gimp_message("Found matching layer, setting active_layer")
+            active_layer = layer
+            frame_index = 0
+
+def update_preview():
+    #pdb.gimp_message("update_preview")
+
+    global window
+    global active_layer
+    global current_frame
+    global display_box
+    global preview_box
+    global frame_index
+    global fps
+
+    active_layer_len = len(active_layer.children)
+    current_frame = active_layer.children[active_layer_len - frame_index - 1]
+    frame_index += 1
+    if frame_index == len(active_layer.children):
+        frame_index = 0
+
+    # Remove anything that might have been rendered before and add new drawable
+    if preview_box is not None:
+        display_box.remove(preview_box)
+    preview_box = gimpui.DrawablePreview(current_frame)
+    display_box.pack_start(preview_box, True, True, 0)
+
+    window.show_all()
+    gobject.timeout_add(1000 / fps, update_preview)
+
+def close_preview_window(ret):
+    pdb.gimp_message("quitting Animation preview plugin")
+    gtk.main_quit()
+
+def is_layer_group(layer):
+    return len(layer.children) > 0
+
+def run_plugin_function(_image, _drawable):
+    pdb.gimp_message("Running Animation Preview plugin")
+
+    global image_ref
+    global layers_list
+    global active_layer
+    global current_frame
+
+    image_ref = _image
+    layers_list = []
+
+    pdb.gimp_message("Image has {} layers".format(len(image_ref.layers)))
+    for layer in image_ref.layers:
+        if is_layer_group(layer):
+            pdb.gimp_message("Adding layer '{}'".format(layer.name))
+            layers_list.append(layer)
+
+    create_preview_window()
+    gtk.main()
+
+######################
+##### Run script #####
+######################
+
+register(
+          "run_plugin_function",
+          "blurb",
+          "Previews animations stored in distinct layer groups",
+          "doomista",
+          "Apache 2 license",
+          "2022",
+          "Layer Group Animation Preview",
+          "*",
+          [
+              (PF_IMAGE, "image", "Input image", None),
+              (PF_DRAWABLE, "drawable", "Input drawable", None),
+          ],
+          [],
+          run_plugin_function, menu="<Image>/Windows")
+main()
+
