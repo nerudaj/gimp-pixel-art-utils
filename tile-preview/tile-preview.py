52260a37b262e95516ef51d5c94b6b9e5f790767
Jakub Neruda <nerudaj@email.cz>
2022-10-26 22:58:46 +0200

    Update tile-preview.py
    Improve performance
    Allow manual updates
    Fix artifacts while zooming

--- tile-preview/tile-preview.py
+++ tile-preview/tile-preview.py
@@ -23,53 +23,58 @@ def zoom_in(widget):
     global zoom_level
     zoom_level = 2.0 * zoom_level
 
+    global FPS
+    if FPS == 0:
+        update_preview()
+
 def zoom_out(widget):
     # pdb.gimp_message("zoom_out")
     global zoom_level
     zoom_level = zoom_level / 2.0
+    
+    global FPS
+    if FPS == 0:
+        update_preview()
+
+def update_preview_wrapper(widget):
+    update_preview()
 
 def update_preview():
     # pdb.gimp_message("update_preview")
 
     global image_ref
-    global temp_img
     global zoom_level
 
     active_layer = image_ref.active_layer
-
-    new_image_width = int(active_layer.width * 3.0 * zoom_level)
-    new_image_height = int(active_layer.height * 3.0 * zoom_level)
     
-    # If image does not exists, create it
-    if temp_img is None:
-        temp_img = pdb.gimp_image_new(
-            new_image_width,
-            new_image_height,
-            image_ref.base_type)
-        temp_img.disable_undo()
+    # Compute base image size
+    new_image_width = int(active_layer.width * 3.0)
+    new_image_height = int(active_layer.height * 3.0)
 
-    # Check whether image needs resizing
-    width_mismatches = new_image_width != temp_img.width
-    height_mismatches = new_image_height != temp_img.height
-    if width_mismatches or height_mismatches:
-        temp_img.resize(new_image_width, new_image_height, 0, 0)
+    # Create temp image that will accomodate new layers
+    temp_img = pdb.gimp_image_new(
+        new_image_width,
+        new_image_height,
+        image_ref.base_type)
+    temp_img.disable_undo()
 
-    # Copy source layer 9 times to new image in tiled, scaled way
-    scaled_layer_width = int(active_layer.width * zoom_level)
-    scaled_layer_height = int(active_layer.height * zoom_level)
+    # Copy source layer 9 times to new image in tiled way
     for y in range(0, 3):
         for x in range(0, 3):
             temp_layer = pdb.gimp_layer_new_from_drawable(active_layer, temp_img)
             temp_img.insert_layer(temp_layer)
-            pdb.gimp_layer_scale_full(
-                temp_layer,
-                scaled_layer_width,
-                scaled_layer_height,
-                False,
-                0) # no interpolation
             temp_layer.translate(
-                x * scaled_layer_width,
-                y * scaled_layer_height)
+                x * active_layer.width,
+                y * active_layer.height)
+
+    # Disable interpolation and zoom the image by scaling    
+    pdb.gimp_context_set_interpolation(0)
+    final_layer = temp_img.flatten()
+    pdb.gimp_layer_scale(
+        final_layer,
+        new_image_width * zoom_level,
+        new_image_height * zoom_level,
+        False)
 
     # Redraw newly created image
     global window
@@ -79,14 +84,15 @@ def update_preview():
     if preview_box is not None:
         display_box.remove(preview_box)
     
-    preview_box = gimpui.DrawablePreview(temp_img.flatten())
-    display_box.pack_start(preview_box, True, True)
+    preview_box = gimpui.DrawablePreview(final_layer)
+    display_box.pack_start(preview_box, True, True, 10)
     
     window.show_all()
 
     # Restart timer for update
     global FPS
-    gobject.timeout_add(1000 / FPS, update_preview)
+    if FPS != 0:
+        gobject.timeout_add(1000 / FPS, update_preview)
 
 def build_gui():
     pdb.gimp_message("build_gui")
@@ -103,11 +109,11 @@ def build_gui():
     window.set_keep_above(True)
 
     global display_box
-    display_box = gtk.HBox();
-    window_box.pack_start(display_box, True, True, vertical_spacing)
+    display_box = gtk.HBox()
+    window_box.pack_start(display_box, True, True, horizontal_spacing)
 
     button_box = gtk.HBox()
-    window_box.pack_start(button_box, True, False, vertical_spacing)
+    window_box.pack_start(button_box, False, False, vertical_spacing)
     
     zoom_button = gtk.Button()
     zoom_button.set_label("+")
@@ -118,6 +124,13 @@ def build_gui():
     unzoom_button.set_label("-")
     unzoom_button.connect("clicked", zoom_out)
     button_box.pack_start(unzoom_button, True, True, horizontal_spacing)
+    
+    global FPS
+    if FPS == 0:
+        update_btn = gtk.Button()
+        update_btn.set_label("Update")
+        update_btn.connect("clicked", update_preview_wrapper)
+        button_box.pack_start(update_btn, True, True, horizontal_spacing)
 
     window.show_all()
 
@@ -133,6 +146,9 @@ def run_tile_preview_plugin_function(_image, _drawable):
 
     global image_ref
     image_ref = _image
+    
+    global zoom_level
+    zoom_level = 128 / image_ref.active_layer.width 
     
     build_gui()
     update_preview()
