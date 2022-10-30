8dfc601ea8b8ea34059236f7ccf15c580fc49855
Jakub Neruda <nerudaj@email.cz>
2022-10-30 09:52:16 +0100

    INDEV: Redo animation preview

--- layer-group-animation-preview/layer-group-animation-preview.py
+++ layer-group-animation-preview/layer-group-animation-preview.py
@@ -5,6 +5,40 @@ import gtk
 import gimpui
 import gobject
 
+class Playback:
+    def __init__(self, frame_count):
+        self.fps = 1
+        self.playing = False
+        self.dirty = True
+        self.frame_index = 0
+        self.frame_count = frame_count
+
+    def start(self):
+        self.playing = True
+
+    def stop(self):
+        self.playing = False
+
+    def next_frame(self):
+        self.frame_index += 1
+        if self.frame_index == self.frame_count:
+            self.frame_index = 0
+        self.dirty = True
+
+    def prev_frame(self):
+        self.frame_index -= 1
+        if self.frame_index < 0:
+            self.frame_index = self.frame_count - 1
+        self.dirty = True
+
+    def update_fps(self, fps):
+        self.fps = fps
+
+    def update(self):
+        if self.playing:
+            self.next_frame()
+        self.dirty = False
+
 # global state
 window = None
 display_box = None
@@ -14,16 +48,47 @@ active_layer = None
 current_frame = None
 frame_index = 0
 fps = 16
+playback = None
 
-def null_callback():
-    print("null_callback called")
+def update_layers_info(image):
+    global layers_list
+    
+    layers_list = []
+    for layer in image_ref.layers:
+        # Is layer group (= has children)
+        if len(layer.children) > 0:
+            # pdb.gimp_message("Adding layer '{}'".format(layer.name))
+            layers_list.append(layer)
+
+def create_button(label, box, spacing):
+    btn = gtk.Button()
+    btn.set_label(label)
+    box.pack_start(btn, True, True, spacing)
+    return btn
 
 def update_fps(widget, entry):
-    pdb.gimp_message("got here")
     global fps
+    global playback
+
     fps_text = entry.get_text()
     fps = int(fps_text)
-    pdb.gimp_message("New fps: {}".format(fps))
+    playback.update_fps(fps)
+
+def start_playback(widget):
+    global playback
+    playback.start()
+
+def stop_playback(widget):
+    global playback
+    playback.stop()
+
+def prev_frame(widget):
+    global playback
+    playback.prev_frame()
+
+def next_frame(widget):
+    global playback
+    playback.next_frame()
 
 def create_preview_window():
     pdb.gimp_message("create_preview_window start")
@@ -42,14 +107,29 @@ def create_preview_window():
     window.connect('destroy',  close_preview_window)
     window_box = gtk.VBox()
     window.add(window_box)
-    window.set_keep_above(True)
 
     display_box = gtk.HBox()
     window_box.pack_start(display_box, True, True, vertical_spacing)
 
+    # Playback controls
+    playback_box = gtk.HBox();
+    window_box.pack_start(playback_box, False, True, vertical_spacing)
+    
+    play_btn = create_button("Play", playback_box, horizontal_spacing)
+    play_btn.connect("clicked", start_playback)
+    
+    stop_btn = create_button("Stop", playback_box, horizontal_spacing)  
+    stop_btn.connect("clicked", stop_playback)
+
+    prev_btn = create_button("Prev", playback_box, horizontal_spacing)
+    prev_btn.connect("clicked", prev_frame)
+
+    next_btn = create_button("Next", playback_box, horizontal_spacing)
+    prev_btn.connect("clicked", next_frame)
+
     # FPS controls
     fps_controls_box = gtk.HBox()
-    window_box.pack_start(fps_controls_box, True, True, vertical_spacing)
+    window_box.pack_start(fps_controls_box, False, True, vertical_spacing)
 
     fps_entry_label = gtk.Label("FPS")
     fps_controls_box.pack_start(fps_entry_label, True, True, horizontal_spacing)
@@ -65,15 +145,15 @@ def create_preview_window():
 
     # Layer group selection
     layer_select_box = gtk.HBox()
-    window_box.pack_start(layer_select_box, True, True, vertical_spacing)
+    window_box.pack_start(layer_select_box, False, True, vertical_spacing)
 
-    layer_combo_label = gtk.Label("Select layer group")
+    layer_combo_label = gtk.Label("Group to play")
     layer_select_box.pack_start(layer_combo_label, True, True, horizontal_spacing)
 
     combox = gtk.combo_box_new_text()
-    combox.connect("changed", active_layer_changed)
+    combox.connect("changed", active_layer_changed, fps_entry)
     for layer in layers_list:
-        pdb.gimp_message(layer.name)
+        # pdb.gimp_message(layer.name)
         combox.append_text(layer.name)
     combox.set_active(0)
     layer_select_box.pack_start(combox, True, True, horizontal_spacing)
@@ -81,68 +161,84 @@ def create_preview_window():
     window.show_all()
     update_preview()
 
-def active_layer_changed(combo):
-    pdb.gimp_message("active_layer_changed to {}".format(combo.get_active_text()))
+def active_layer_changed(combo, fps_entry):
+    pdb.gimp_message("active_layer_changed({})"
+        .format(combo.get_active_text()))
 
     global layers_list
     global active_layer
-    global frame_index
+    global playback
 
     for layer in layers_list:
         if layer.name == combo.get_active_text():
             pdb.gimp_message("Found matching layer, setting active_layer")
             active_layer = layer
-            frame_index = 0
+            playback = Playback(len(active_layer.children))
+            update_fps(None, fps_entry)
+    
+    pdb.gimp_message("Is playback none: {}".format(playback is None))
+
+def get_scaled_layer(layer, scale):
+    global image_ref
+    temp_img = pdb.gimp_image_new(
+        layer.width,
+        layer.height,
+        image_ref.base_type)
+    temp_img.disable_undo()
+    
+    temp_layer = pdb.gimp_layer_new_from_drawable(
+        layer, temp_img)
+    temp_img.insert_layer(temp_layer)
+    pdb.gimp_context_set_interpolation(0)
+    pdb.gimp_layer_scale(
+        temp_layer,
+        layer.width * scale,
+        layer.height * scale,
+        False)
+    
+    return temp_layer
 
 def update_preview():
-    #pdb.gimp_message("update_preview")
-
-    global window
-    global active_layer
+    pdb.gimp_message("update_preview")
+    
+    global playback
     global current_frame
+    global active_layer
+
+    if not (playback.playing or playback.dirty):
+        gobject.timeout_add(1000 / playback.fps, update_preview)
+        return
+
+    playback.update()
+    
+    active_layer_len = len(active_layer.children)
+    current_frame = active_layer.children[active_layer_len - playback.frame_index - 1]
+    
     global display_box
     global preview_box
-    global frame_index
-    global fps
-
-    active_layer_len = len(active_layer.children)
-    current_frame = active_layer.children[active_layer_len - frame_index - 1]
-    frame_index += 1
-    if frame_index == len(active_layer.children):
-        frame_index = 0
-
-    # Remove anything that might have been rendered before and add new drawable
+    global window
+    
     if preview_box is not None:
         display_box.remove(preview_box)
-    preview_box = gimpui.DrawablePreview(current_frame)
-    display_box.pack_start(preview_box, True, True, 0)
 
+    preview_box = gimpui.DrawablePreview(
+        get_scaled_layer(current_frame, 10))
+    display_box.pack_start(preview_box, True, True, 0)
     window.show_all()
-    gobject.timeout_add(1000 / fps, update_preview)
+    gobject.timeout_add(1000 / playback.fps, update_preview)
 
 def close_preview_window(ret):
     pdb.gimp_message("quitting Animation preview plugin")
     gtk.main_quit()
 
-def is_layer_group(layer):
-    return len(layer.children) > 0
-
 def run_plugin_function(_image, _drawable):
     pdb.gimp_message("Running Animation Preview plugin")
 
     global image_ref
-    global layers_list
-    global active_layer
-    global current_frame
-
     image_ref = _image
-    layers_list = []
 
     pdb.gimp_message("Image has {} layers".format(len(image_ref.layers)))
-    for layer in image_ref.layers:
-        if is_layer_group(layer):
-            pdb.gimp_message("Adding layer '{}'".format(layer.name))
-            layers_list.append(layer)
+    update_layers_info(image_ref)
 
     create_preview_window()
     gtk.main()
