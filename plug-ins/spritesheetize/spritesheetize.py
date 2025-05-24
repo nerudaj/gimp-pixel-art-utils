3a532d9598bc4f252e949f97a88faa6501b1db07
Jakub Neruda <nerudaj@email.cz>
2025-05-24 16:14:37 +0200

    add spritesheetize functionality

--- plug-ins/spritesheetize/spritesheetize.py
+++ plug-ins/spritesheetize/spritesheetize.py
@@ -33,7 +33,7 @@ class Vector2d:
     def get_scaled(self, factor: int) -> Self:
         return Vector2d(self.x * factor, self.y * factor)
     
-    def to_json(self) -> dict[str, str]:
+    def to_json(self) -> dict[str, int]:
         return {
             "width": int(self.x),
             "height": int(self.y)
@@ -44,7 +44,7 @@ class Box:
         self.position = position
         self.size = size
 
-    def to_json(self) -> dict[str, str]:
+    def to_json(self) -> dict[str, int]:
         return {
             "left": int(self.position.x),
             "top": int(self.position.y),
@@ -83,12 +83,14 @@ def copy_layer_to_image(layer: Gimp.Layer,
                         x: int, y: int):
     temp_layer = Gimp.Layer.new_from_drawable(layer, target_image)
     temp_layer.set_visible(True)
-    temp_layer.add_alpha()
+
+    if not temp_layer.is_group():
+        temp_layer.add_alpha()
 
     target_image.insert_layer(temp_layer, None, 0)
     temp_layer.transform_translate(x, y)
 
-def write_obj_to_file_as_json(obj, filename):
+def write_obj_to_file_as_json(obj, filename: str):
     fp = open(filename, "wt")
     json.dump(obj, fp, indent=4)
     fp.close()
@@ -110,6 +112,35 @@ def export_tileset_annotations(filename: str,
 
     write_obj_to_file_as_json(annotation, filename + ".clip")
 
+def export_spritesheet_annotations(filename: str,
+                                   frame_size: Vector2d,
+                                   options: ExportOptions,
+                                   fitted_groups: list[list[Gimp.GroupLayer]]):
+    annotation = {
+        "defaults": {
+            "frame": frame_size.get_scaled(options.scaling_factor).to_json(),
+            "spacing": frame_size.get_scaled(options.scaling_factor).to_json(),
+        },
+        "states": []
+    }
+
+    for rowIdx in range(len(fitted_groups)):
+        startColumnIdx = 0
+        for group in fitted_groups[rowIdx]:
+            group_len = len(group.get_children())
+            annotation["states"].append({
+                "name": group.get_name(),
+                "bounds": Box(Vector2d(options.offset.x + startColumnIdx * (options.spacing.x + frame_size.x),
+                                       options.offset.y + rowIdx * (options.spacing.y + frame_size.y)).get_scaled(options.scaling_factor),
+                              Vector2d(group_len * frame_size.x + (group_len - 1) * options.spacing.x,
+                                       frame_size.y).get_scaled(options.scaling_factor)).to_json(),
+                "nframes": len(group.get_children())
+            })
+
+            startColumnIdx += group_len
+    
+    write_obj_to_file_as_json(annotation, filename + ".anim")
+
 def get_tileset_row_count(layer_count: int) -> tuple[int, int]:
     n_tiles_per_row = int(round(math.sqrt(layer_count)))
     n_rows = int(layer_count / n_tiles_per_row)
@@ -117,25 +148,26 @@ def get_tileset_row_count(layer_count: int) -> tuple[int, int]:
         n_rows += 1 # There are some extra tiles
     return (n_tiles_per_row, n_rows)
 
+def create_out_image(in_image: Gimp.Image, options: ExportOptions, tiles_per_row: int, row_count: int) -> Gimp.Image:
+    return Gimp.Image.new(tiles_per_row * in_image.get_width() + 2 * options.offset.x + (tiles_per_row - 1) * options.spacing.x,
+                          row_count * in_image.get_height() + 2 * options.offset.y + (row_count - 1) * options.spacing.y,
+                          in_image.get_base_type())
+
 def tilesetize(image: Gimp.Image,
                options: ExportOptions) -> tuple[Gimp.Image, int, int]:
     layers = image.get_layers()
 
     (tiles_per_row, row_count) = get_tileset_row_count(len(layers))
     
-    framew = image.get_width()
-    frameh = image.get_height()
-    
-    out_image = Gimp.Image.new(
-        tiles_per_row * framew + 2 * options.offset.x + (tiles_per_row - 1) * options.spacing.x,
-        row_count * frameh + 2 * options.offset.y + (row_count - 1) * options.spacing.y,
-        image.get_base_type())
+    out_image = create_out_image(image, options, tiles_per_row, row_count)
     
     for idx in range(0, len(layers)):
+        columnIdx = idx % tiles_per_row
+        rowIdx = math.floor(idx / tiles_per_row)
         copy_layer_to_image(layers[idx],
                             out_image,
-                            options.offset.x + (idx % tiles_per_row) * (framew + options.spacing.x),
-                            options.offset.y + math.floor(idx / tiles_per_row) * (frameh + options.spacing.y))
+                            options.offset.x + columnIdx * (image.get_width() + options.spacing.x),
+                            options.offset.y + rowIdx * (image.get_height() + options.spacing.y))
     
     Gimp.context_set_interpolation(Gimp.InterpolationType.LINEAR)
     out_image.scale(out_image.get_width() * options.scaling_factor,
@@ -143,9 +175,73 @@ def tilesetize(image: Gimp.Image,
 
     return (out_image, tiles_per_row, row_count)
 
+def get_next_best_fitting_group(groups: list[Gimp.GroupLayer],
+                                max_tiles_per_row: int,
+                                currently_used_tiles: int) -> Gimp.GroupLayer | None:
+    target_score = max_tiles_per_row - currently_used_tiles
+    score = -1
+    best_idx = -1
+    for idx in range(len(groups)):
+        current_score = len(groups[idx].get_children())
+        if current_score > target_score:
+            continue
+
+        if score == -1 or score < current_score:
+            score = current_score
+            best_idx = idx
+    
+    if best_idx != -1:
+        result = groups.pop(best_idx)
+        return result
+    
+    return None
+
+def fit_groups(groups: list[Gimp.GroupLayer]) -> list[list[Gimp.GroupLayer]]:
+    group_lens = list(len(g.get_children()) for g in groups)
+    max_group_len = max(group_lens)
+
+    rows: list[list[Gimp.GroupLayer]] = [[]]
+    current_row_idx = 0
+    current_row_usage = 0
+    while len(groups) > 0:
+        group = get_next_best_fitting_group(groups, max_group_len, current_row_usage)
+        if group is None:
+            rows.append(list())
+            current_row_idx += 1
+            current_row_usage = 0
+            continue
+
+        rows[current_row_idx].append(group)
+        current_row_usage += len(group.get_children())
+
+    return rows
+
 def spritesheetize(image: Gimp.Image,
-                   options: ExportOptions):
-    pass
+                   options: ExportOptions) -> tuple[Gimp.Image, list[list[Gimp.GroupLayer]]]:
+    groups: list[Gimp.GroupLayer] = list(filter(lambda x: x.is_group_layer(), image.get_layers()))
+
+    if len(groups) == 0:
+        log("No groups to export!")
+        return (None, [])
+
+    fitted_groups = fit_groups(groups)
+    tiles_per_row = len(fitted_groups[0][0].get_children()) # First row always contains the longest animation
+    row_count = len(fitted_groups)
+
+    out_image = create_out_image(image, options, tiles_per_row, row_count)
+
+    for rowIdx in range(len(fitted_groups)):
+        columnIdx = 0
+        for group in fitted_groups[rowIdx]:
+            for layer in group.get_children():
+                copy_layer_to_image(layer,
+                                    out_image,
+                                    options.offset.x + columnIdx * (image.get_width() + options.spacing.x),
+                                    options.offset.y + rowIdx * (image.get_height() + options.spacing.y))
+                columnIdx += 1
+    
+    return (out_image, fitted_groups)
+
 
 def spritify_run(procedure: Gimp.Procedure,
                  run_mode: Gimp.RunMode,
@@ -167,17 +263,29 @@ def spritify_run(procedure: Gimp.Procedure,
                             False)
 
 
-    (out_image, tiles_per_row, row_count) = tilesetize(image, options)
+    if do_spritesheetize:
+        (out_image, fitted_groups) = spritesheetize(image, options)
+
+        if out_image is None:
+            return procedure.new_return_values(Gimp.PDBStatusType.EXECUTION_ERROR, None)
+        
+        export_spritesheet_annotations(outfile.get_path(),
+                                       Vector2d(image.get_width(), image.get_height()),
+                                       options,
+                                       fitted_groups)
+
+    else:
+        (out_image, tiles_per_row, row_count) = tilesetize(image, options)
+        export_tileset_annotations(outfile.get_path(),
+                                   Vector2d(image.get_width(), image.get_height()),
+                                   options,
+                                   tiles_per_row, row_count)
 
     Gimp.file_save(Gimp.RunMode.NONINTERACTIVE,
                    out_image,
                    outfile,
                    None)
     
-    export_tileset_annotations(outfile.get_path(),
-                               Vector2d(image.get_width(), image.get_height()),
-                               options,
-                               tiles_per_row, row_count)
     return procedure.new_return_values(Gimp.PDBStatusType.SUCCESS, None)
 
 class Spritify (Gimp.PlugIn):
@@ -201,8 +309,6 @@ class Spritify (Gimp.PlugIn):
         procedure.add_menu_path(plug_in_path)
         procedure.set_documentation(plug_in_docs, None)
 
-        # Add arguments
-        # TODO: detect whether current image has layer groups
         procedure.add_file_argument("outfile",
                                     "Choose output file",
                                     None,
@@ -212,7 +318,7 @@ class Spritify (Gimp.PlugIn):
                                     GObject.ParamFlags.READWRITE)
         
         procedure.add_boolean_argument("groups-are-animations",
-                                       "Layer groups are animation clips",
+                                       "Export layer groups as animation clips",
                                        None,
                                        False,
                                        GObject.ParamFlags.READWRITE)
